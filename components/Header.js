0db7d69db597fee487293883e924ab2cefcbe7f7
Mehrab Uddin <mehrab.uddin905@gmail.com>
2021-09-13 15:36:19 +0500

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1>header</h1>
+      <Image src='https://links.papareact.com/ua6' width={200} height={100} />
+    </div>
+  );
+};
